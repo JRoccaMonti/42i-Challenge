9cb5a36de3d8648e6f29f89338eb0a6a26c84b8c
JRoccaMonti <Rocca.Jano@gmail.com>
2024-09-06 15:00:09 -0300

    estructuracion

--- src/Helpers/TwoNumberSum/twoNumberSum.test.tsx
+++ src/Helpers/TwoNumberSum/twoNumberSum.test.tsx
@@ -1,14 +1,14 @@
 import { renderHook } from '@testing-library/react';
 
-import useTwoNumberSum from "./index.tsx";
+import TwoNumberSum from "./index.tsx";
 
-describe('useTwoNumberSum',()=>{
+describe('TwoNumberSum',()=>{
     it('should return the correct pair when a valid pair exists', () => {
-      const { result } = renderHook(() => useTwoNumberSum([1,2,3,4], 5));
+      const { result } = renderHook(() => TwoNumberSum([1,2,3,4], 5));
       expect(result.current).toEqual([2,3]);
     });
     it('should return the correct pair when a valid pair not exists', () => {
-      const { result } = renderHook(() => useTwoNumberSum([1,2,3,4], 10));
+      const { result } = renderHook(() => TwoNumberSum([1,2,3,4], 10));
       expect(result.current).toEqual([]);
     });
 });
